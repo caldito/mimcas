80f50509f4c500b3c27cbbe74ee15c6101ba36b6
Pablo Gómez-Caldito <pablo@streamsets.com>
2022-01-01 13:11:15 +0100

    Start building http rest api

--- cmd/kv-store/main.go
+++ cmd/kv-store/main.go
@@ -2,21 +2,45 @@ package main
 
 import (
 	// from other places
+	"net/http"
+	"github.com/go-chi/chi/v5"
 	"flag"
-	"fmt"
+	// 	"fmt"
 	// "os"
 	// "time"
 )
 
+func getArticle(w http.ResponseWriter, r *http.Request) {
+	keyParam := chi.URLParam(r, "key")
+	//slugParam := chi.URL(r, "slug")
+	//article, err := database.GetArticle(date, slug)
+  
+	//if err != nil {
+	//  w.WriteHeader(422)
+	//  w.Write([]byte(fmt.Sprintf("error fetching article %s-%s: %v", dateParam, slugParam, err)))
+	//  return
+	//}
+	//
+	//if article == nil {
+	//  w.WriteHeader(404)
+	//  w.Write([]byte("article not found"))
+	//  return
+	//}
+	w.Write([]byte("The key is: " + keyParam))
+}
+
 func main() {
 	//var repo string
-	var port int
+	var port string
 	//flag.StringVar(&repo, "repo", "", "url of the repository")
-	flag.IntVar(&port, "port", 8080, "port to listen to")
+	flag.StringVar(&port, "port", "8080", "port to listen to")
 	flag.Parse()
+	r := chi.NewRouter()
+	r.Get("/value/{key}", getArticle)
+	
+	http.ListenAndServe(":" + port, r)
 	// if repo == "" {
 	// 	fmt.Println("Exiting, repo flag is not provided")
 	// 	os.Exit(2)
 	// }
-	fmt.Println("Hello world")
 }
