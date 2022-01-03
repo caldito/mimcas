7a3ab8f52b3d308ec60e0e7900695cc41555e183
Pablo Gómez-Caldito <pablo@streamsets.com>
2022-01-03 21:40:30 +0100

    Refactor connection handling and allow lowercase commands

--- cmd/kv-store/main.go
+++ cmd/kv-store/main.go
@@ -13,6 +13,49 @@ import (
 
 var m = make(map[string]string)
 
+func set(params []string) string {
+	response := ""
+	if len(params) == 3 {
+		m[params[1]] = params[2]
+		response = "OK\n"
+	} else {
+		response = "ERR syntax error\n"
+	}
+	return response
+}
+
+func get(params []string) string {
+	response := ""
+	if 2 == len(params) {
+		value := m[params[1]]
+		if value == "" {
+			response = "(nil)\n"
+		} else {
+			response = value + "\n"
+		}
+	} else {
+		response = "ERR syntax error\n"
+	}
+	return response
+}
+
+func mget(params []string) string {
+	response := ""
+	if 2 <= len(params) {
+		for _, key := range params[1:] {
+			value := m[key]
+			if value == "" {
+				response = response + "(nil)\n"
+			} else {
+				response = response + value + "\n"
+			}
+		}
+	} else {
+		response = "ERR syntax error\n"
+	}
+	return response
+}
+
 func handleConnection(c net.Conn) {
 	fmt.Printf("Serving %s\n", c.RemoteAddr().String())
 	for {
@@ -24,40 +67,15 @@ func handleConnection(c net.Conn) {
 		data := strings.TrimSpace(string(netData))
 		params := strings.Split(data, " ")
 		response := ""
-		if params[0] == "SET" {
-			if len(params) == 3 {
-				m[params[1]] = params[2]
-				response = "OK\n"
-			} else {
-				response = "ERR syntax error\n"
-			}
-		} else if params[0] == "GET" {
-			if 2 == len(params) {
-				value := m[params[1]]
-				if value == "" {
-					response = "(nil)\n"
-				} else {
-					response = value + "\n"
-				}
-			} else {
-				response = "ERR syntax error\n"
-			}
-		} else if params[0] == "MGET" {
-			if 2 <= len(params) {
-				for _, key := range params[1:] {
-					value := m[key]
-					if value == "" {
-						response = response + "(nil)\n"
-					} else {
-						response = response + value + "\n"
-					}
-				}
-			} else {
-				response = "ERR syntax error\n"
-			}
-		} else if params[0] == "QUIT" {
+		if params[0] == "SET" || params[0] == "set" {
+			response = set(params)
+		} else if params[0] == "GET" || params[0] == "get" {
+			response = get(params)
+		} else if params[0] == "MGET" || params[0] == "mget" {
+			response = mget(params)
+		} else if params[0] == "QUIT" || params[0] == "quit" {
 			break
-		} else if params[0] == "PING" {
+		} else if params[0] == "PING" || params[0] == "ping" {
 			response = "PONG\n"
 		} else {
 			response = "ERR unknown command\n"
